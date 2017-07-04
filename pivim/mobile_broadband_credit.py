4eb7d391e9953c28bb915188e8cbcfda65bf23e7
Graham Smith <graham@pleasereleaseme.net>
2017-07-04 22:43:51 +0100

    Update  mobile_broadband_credit

--- pivim/mobile_broadband_credit.py
+++ pivim/mobile_broadband_credit.py
@@ -1,3 +1,5 @@
+"""The
+"""
 import requests
 from lxml import html
 
@@ -5,9 +7,10 @@ USERNAME = "07465253874"
 PASSWORD = "eq*mP@qCkxT6f$5C"
 
 LOGIN_URL = "https://www.three.co.uk/My3Account/Login"
-URL = "https://www.three.co.uk/New_My3/Account_balance?id=My3_CheckYourBalanceLink"
+URL = "https://www.three.co.uk/New_My3/Account_balance"
 
 def main():
+    """The"""
     session_requests = requests.session()
 
     # Get login csrf token
@@ -16,21 +19,13 @@ def main():
     authenticity_token = list(set(tree.xpath("//input[@name='lt']/@value")))[0]
 
     # Create payload
-    payload = {
-        "username": USERNAME, 
-        "password": PASSWORD, 
-        "csrfmiddlewaretoken": authenticity_token
-    }
-
-    # Perform login
+    payload = {"username": USERNAME, "password": PASSWORD, "lt": authenticity_token}
+    
     result = session_requests.post(LOGIN_URL, data = payload, headers = dict(referer = LOGIN_URL))
 
-    # Scrape url
     result = session_requests.get(URL, headers = dict(referer = URL))
     tree = html.fromstring(result.content)
-    bucket_names = tree.xpath("//div[@class='repo-list--repo']/a/text()")
 
-    print(bucket_names)
 
 if __name__ == '__main__':
     main()
