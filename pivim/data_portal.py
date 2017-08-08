2bb3b5693e91121399013710228e967db9bcd441
Graham Smith <graham@pleasereleaseme.net>
2017-08-08 23:28:10 +0100

    Update data_portal

--- pivim/data_portal.py
+++ pivim/data_portal.py
@@ -1,13 +1,15 @@
 """
 The
 """
-import datetime as d
+import datetime as dt
 from ISStreamer.Streamer import Streamer
 
 def upload_data(stream_name, temperature):
     """The """
-    streamer = Streamer(bucket_name="PiViM-{}".format(d.date()), \
-                        bucket_key="pivim_{}".format(d.date()), \
+    today = dt.date.today()
+
+    streamer = Streamer(bucket_name="PiViM-{}".format(today), \
+                        bucket_key="pivim_{}".format(today), \
                         access_key="8077mAn79ul7pd5sf2bvdyfjaoAQCBZ4")
 
     streamer.log(stream_name, temperature)
