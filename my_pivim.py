2057789310df8a3cc08de7158c6c2abf3888a8a6
Graham Smith <graham@pleasereleaseme.net>
2017-08-30 19:27:47 +0100

    U

--- my_pivim.py
+++ my_pivim.py
@@ -4,7 +4,7 @@ import time
 from pivim import mobile_broadband as mb
 from pivim import control_panel as cp
 from pivim import data_portal as dp
-#from pivim import temperature as t
+from pivim import temperature as t
 
 def main():
     """Evolving picture of how all the different code modules will work together"""
@@ -18,12 +18,12 @@ def main():
             signalbar = mobile.signalbar
             network_type = mobile.network_type
 
-            latest_temp = "20" # t.read_temp()
+            latest_temp = t.read_temp()
 
             cp.clear_screen()
             cp.message_right_top(network_type)
             cp.message_right_middle('*' * int(signalbar))
-            cp.message_left_middle(latest_temp)
+            cp.message_left_middle(str(latest_temp))
 
             dp.upload_data("Temperature", latest_temp)
             time.sleep(5)
