5409778f1f7212b3c6a6c1887c815c5bef4574e9
Graham Smith <graham@pleasereleaseme.net>
2017-08-30 19:14:58 +0100

    My_pivim update

--- my_pivim.py
+++ my_pivim.py
@@ -2,13 +2,13 @@
 """DocString"""
 import time
 from pivim import mobile_broadband as mb
-#from pivim import control_panel as cp
+from pivim import control_panel as cp
 from pivim import data_portal as dp
 from pivim import temperature as t
 
 def main():
     """Evolving picture of how all the different code modules will work together"""
-    #cp.display_config()
+    cp.display_config()
 
     try:
         while True:
@@ -20,17 +20,16 @@ def main():
 
             latest_temp = t.read_temp()
 
-            #cp.clear_screen()
-            #cp.message_right_top(network_type)
-            #cp.message_right_middle('*' * int(signalbar))
+            cp.clear_screen()
+            cp.message_right_top(network_type)
+            cp.message_right_middle('*' * int(signalbar))
+            cp.message_right_middle(latest_temp)
 
-            latest_temp = t.read_temp()
             dp.upload_data("Temperature", latest_temp)
             time.sleep(5)
 
     except KeyboardInterrupt:
-        #cp.display_off()
-        pass
+        cp.display_off()
 
 if __name__ == '__main__':
     main()
