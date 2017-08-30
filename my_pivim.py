82541380516d1ee9dbfcd3aeaa83746f2ada3177
Graham Smith <graham@pleasereleaseme.net>
2017-08-30 19:48:05 +0100

    U

--- my_pivim.py
+++ my_pivim.py
@@ -10,6 +10,8 @@ def main():
     """Evolving picture of how all the different code modules will work together"""
     cp.display_config()
 
+    highest_temp = 0
+
     try:
         while True:
             mobile = mb.MobileBroadband()
@@ -19,11 +21,13 @@ def main():
             network_type = mobile.network_type
 
             latest_temp = t.read_temp()
+            highest_temp = latest_temp if latest_temp > highest_temp
 
             cp.clear_screen()
             cp.message_right_top(network_type)
             cp.message_right_middle('*' * int(signalbar))
             cp.message_left_middle(str(latest_temp))
+            cp.message_left_bottom(str(highest_temp))
 
             dp.upload_data("Temperature", latest_temp)
             time.sleep(5)
