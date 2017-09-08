c32e78ac3ca3ab8707a773f91844a90d4dcda5e6
Graham Smith <graham@pleasereleaseme.net>
2017-09-08 15:07:07 +0100

    u

--- pivim/data_portal.py
+++ pivim/data_portal.py
@@ -6,10 +6,10 @@ from ISStreamer.Streamer import Streamer
 
 def upload_data(stream_name, temperature):
     """The """
-    today = dt.date.ctime()
+    today = dt.date.today()
 
     streamer = Streamer(bucket_name="PiViM-{}".format(today), \
-                        bucket_key="pivim_{}".format(today), \
+                        bucket_key="pivim_t{}".format(today), \
                         access_key="8077mAn79ul7pd5sf2bvdyfjaoAQCBZ4")
 
     streamer.log(stream_name, temperature)
