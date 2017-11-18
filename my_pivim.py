27a0ab4bc29888709d733947d83ac46892f2a6f4
Graham Smith <graham@pleasereleaseme.net>
2017-11-18 13:43:41 +0000

    u

--- my_pivim.py
+++ my_pivim.py
@@ -36,7 +36,7 @@ def main(access_key):
 
             dp.upload_temperature_data(access_key, latest_temp)
 
-            time.sleep(5)
+            time.sleep(10)
 
         except KeyboardInterrupt:
             cp.display_off()
